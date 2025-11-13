f102d40efe043616a1f14a2edea73b3d319f9a8e
Simeon Bentke <bentkesimeon@gmail.com>
2025-11-12 21:02:28 -0600

    add class 'one_qubit_trivial_system_quantum'

--- vqe_simeon.py
+++ vqe_simeon.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import Operator, Statevector
+
 
 class base():
     def __init__(self):
@@ -7,18 +10,38 @@ class base():
         self.max_iter=1000
         self.store_history_energy=[]
         self.store_history_theta=[]
+        self.store_history_qubit=[]
 
     def run(self):
         for _ in range(self.max_iter):
             self.store_history_energy.append(self.energy(self.state_update(self.theta)))
-            #self.store_history_theta.append(self.theta)
+            self.store_history_theta.append(self.theta)
+            self.store_history_qubit.append(self.state_update(self.theta).data)
             self.step_theta()
-
+        self.plots()
+    
     def state_update(self,theta):
-        return self.unitary(theta)@self.state
+        raise NotImplementedError("Implement this method in a subclass")
 
     def step_theta(self):
         self.theta=self.theta-self.step_size()*self.gradient()
+    
+    def plots(self):
+        plt.title('theta(green) and energy(red) evolution')
+        plt.plot(s.store_history_theta, 'g')
+        plt.plot(s.store_history_energy, 'r')
+        plt.show()
+
+
+        x_vals=[x for x, y in s.store_history_qubit]
+        y_vals=[y for x, y in s.store_history_qubit]
+        plt.plot(y_vals)
+        plt.plot(x_vals)
+        plt.title('state-evolution')
+        plt.show()
+
+
+
 
 class step_size_const(base):
     def step_size(self):
@@ -50,20 +73,47 @@ class one_qubit_trivial_system(gradiant_clas,step_size_const):
         self.H=-X-Z
         self.state=np.array([1,0])
         self.theta=np.pi/2
+
     def unitary(self,theta):
         c=np.cos(theta/2)
         s=np.sin(theta/2)
         u=np.array([[c,s],[-s,c]])
         return u
+    
+    def state_update(self,theta):
+        return self.unitary(theta)@self.state
+    
     def energy(self,state):
         energy=np.vdot(state,self.H @ state)
         return energy
+    
 
-s=one_qubit_trivial_system()
+class one_qubit_trivial_system_quantum(gradiant_clas,step_size_const):
+    def __init__(self):
+        super().__init__()
+        X=np.array([[0,1],[1,0]])
+        Z=np.array([[1,0],[0,-1]])
+        self.H=Operator(-X-Z)
+        self.state=Statevector.from_instruction(QuantumCircuit(1))
+        #print(Statevector.from_instruction(self.state))
+        self.theta=np.pi/2
+    def unitary(self,theta):
+        c=np.cos(theta/2)
+        s=np.sin(theta/2)
+        u=np.array([[c,s],[-s,c]])
+        return Operator(u)
+    
+    def state_update(self,theta):
+        u=self.unitary(theta)
+        return self.state.evolve(u,qargs=[0])
+    
+    def energy(self,state):
+        energy=np.vdot(state.data,self.H.data @ state.data)
+        return energy
+
+s=one_qubit_trivial_system_quantum()
 s.run()
 
 
 
-plt.plot(s.store_history_theta, 'g')
-plt.plot(s.store_history_energy, 'r')
-plt.show()
+
